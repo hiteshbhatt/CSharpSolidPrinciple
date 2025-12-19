700d23193633028beeaf5bf0a216744f93c1eb9d
hiteshbhatt <hiteshbhatt2122@gmail.com>
2025-12-19 16:19:43 +0530

    Dependency inversion Principle

---
+++ DIP/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public interface ILogger
+{
+    void Log(string message);
+}
+
+public class FileLogger : ILogger // low level module
+{
+    public void Log(string message)
+    {
+        File.WriteAllText(@"c:\error.text",message);
+    }
+}
+
+public class ConsoleLogger: ILogger
+{
+    public void Log(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+public class MemberShip // high level module
+{
+    ILogger logger; // abstraction
+    public MemberShip(ILogger logger)
+    {
+        this.logger = logger;
+    }
+    public void Add() 
+    {
+        //ToDO:: any operation
+
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+           
+
+            logger.Log(ex.ToString());
+        }
+    }
+}
+
+
