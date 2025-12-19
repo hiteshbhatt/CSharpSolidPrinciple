614c2e37a3a25729e1bb5fd3993f9ed5be675460
hiteshbhatt <hiteshbhatt2122@gmail.com>
2025-12-19 14:48:49 +0530

    OCP and SRP pringciple

---
+++ OCP/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace OCP
+{
+
+    //we have an required to add more membership pro and plus
+    public class MemberShip
+    {
+        //old way
+        //If we have to add new membership then we have to change this class
+
+        //public int GetTrainings(string memberShipType)
+        //{
+        //    if (memberShipType == "Plus") {
+
+        //        return 4;
+        //    }
+        //    else if(memberShipType=="Pro"){
+        //        return 5;
+
+        //    }
+        //    return 2;
+        //}
+
+        public virtual int GetTrainings()
+        {
+            return 2;
+        }
+    }
+
+    //new way we will use inheritance
+    public class PlusMembership: MemberShip
+    {
+            public override int GetTrainings()
+            {
+                return 4;
+            }
+    }
+
+        public class ProMembership : MemberShip
+        {
+            public override int GetTrainings()
+            {
+                return 5;
+            }
+        }
+        internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ SolidPrinciples/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public class MemberShip
+{
+    public void Add() // it is related to membership
+    {
+        //ToDO:: any operation
+
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+            // File.WriteAllText(@"c:\error.txt",ex.Message); // this is another reponsibility to log error it should be perform by another class Logger
+
+            FilerLogger.LogError("erro");
+        }
+    }
+}
+
+public class FilerLogger // this is correction
+{
+    public static void LogError(string error)
+    {
+        File.WriteAllText(@"c:\error.txt", error);
+    }
+}
