96176f30b0b6972c2ecb58a93ddfacadf8c7009a
hiteshbhatt <hiteshbhatt2122@gmail.com>
2025-12-30 12:51:51 +0530

    Extention method

---
+++ ExtentionMethod/Class1.cs
@@ -0,0 +1,26 @@
+﻿namespace ExtentionMethod
+{
+
+    /**
+     * Create extention method for string 
+     * 
+     */
+    public class StringHelper
+    {
+
+        public static string ChangeFirstLetter(string str)
+        {
+
+            if (str.Length > 0)
+            {
+                char[] charArray = str.ToCharArray();
+                charArray[0] = char.IsUpper(charArray[0]) ?
+               char.ToLower(charArray[0]) : char.ToUpper(charArray[0]);
+                return new string(charArray);
+
+            }
+            return str;
+        }
+
+    }
+}
